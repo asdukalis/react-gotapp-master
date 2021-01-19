771ce6a0c5d91c744f343cf747980e7c1d49f30b
Andrey <asdukalis@gmail.com>
2021-01-19 08:52:35 +0200

    Sec React

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+    <div className="loadingio-spinner-spin-yptc5qkck8k">
+      <div className="ldio-9a36ikican6">
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
---
+++ src/services/gotService.js
@@ -0,0 +1,79 @@
+export default class GotService {
+  constructor() {
+    this._apiBase = 'https://www.anapioficeandfire.com/api';
+  }
+
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+
+    if (!res.ok) {
+      throw new Error(
+        `Could not fetch ${this._apiBase}${url}, status: ${res.status}`
+      );
+    }
+
+    return await res.json();
+  }
+
+  //!Characters
+  async getAllCharacters() {
+    const res = await this.getResource('/characters?page=5&pageSize=10');
+    return res.map(this._transformCharacter);
+  }
+
+  async getCharacter(id) {
+    const character = await this.getResource(`/characters/${id}`);
+    return this._transformCharacter(character);
+  }
+
+  //!Books
+  async getAllBooks() {
+    const res = await this.getResource('/books/');
+    return res.map(this._transformCharacter);
+  }
+
+  async getBook(id) {
+    const book = await this.getResource(`/books/${id}`);
+    return this._transformBook(book);
+  }
+
+  //!Houses
+  async getAllHouses() {
+    const res = await this.getResource('/houses/');
+    return res.map(this._transformHouse);
+  }
+
+  async getHouse(id) {
+    const house = await this.getResource(`/houses/${id}`);
+    return this._transformHouse(house);
+  }
+
+  _transformCharacter(char) {
+    return {
+      name: char.name,
+      gender: char.gender,
+      born: char.born,
+      died: char.died,
+      culture: char.culture,
+    };
+  }
+  _transformHouse(house) {
+    return {
+      name: house.name,
+      region: house.region,
+      words: house.words,
+      titles: house.titles,
+      overlord: house.overlord,
+      ancestralWeapons: house.ancestralWeapons,
+    };
+  }
+
+  _transformBook(book) {
+    return {
+      name: book.name,
+      numberOfPages: book.numberOfPages,
+      publiser: book.publiser,
+      released: book.released,
+    };
+  }
+}
